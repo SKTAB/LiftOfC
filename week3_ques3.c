2b0610cb0735fb3649c46e8098a83abbdd1bc045
SKTAB <99458038+SKTAB@users.noreply.github.com>
2022-05-01 08:33:35 -0700

    Create week3_ques3.c

---
+++ week3_ques3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int Even(int num)
+{
+    return(num);
+}
+int main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    if(Even(num))
+    {
+        printf("The number is even.");
+    }
+    else
+    {
+        printf("The number is odd.");
+    }
+
+    return 0;
+}
